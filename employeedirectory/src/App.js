c0477c2b646497859a09539de6507eab4d354b51
Samya Fiki <crystalgrey@Samyas-MBP.fios-router.home>
2021-03-24 01:55:43 -0400

    Hello world works as a start with its component.

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; //name of the module
+import HelloReact from "./components/HelloReact"; //the folder area right here. Do not need to put .js
+import './App.css'; //How to link css
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App() { //this is called from index.js
+  return <HelloReact />; //name of that component without .js at the end. comes from helloreact.js file
+
 }
 
-export default App;
+export default App; //export the app component(function)
