5826be8f260f5a9d37c3a75d62a4fda982698634
Samya Fiki <crystalgrey@Samyas-MBP.fios-router.home>
2021-03-27 21:59:31 -0400

    Got api working again with table population.

--- employeedirectory/src/components/EmployeeCard/index.js
+++ employeedirectory/src/components/EmployeeCard/index.js
@@ -1,31 +1,53 @@
 import React from "react";
 import "./style.css";
 
-function EmployeeCard(employeeInfo) {
+function EmployeeCard({ employeeInfo }) {
   // const working = dataSet.length > 0
   // console.log({working})
 
   // const info = {
   //     console.log(working && dataSet)
   // }
-  console.log(employeeInfo)
-  
+  console.log(employeeInfo);
+
   return (
     <table className="table table-hover">
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
+          <th scope="col"></th>
+          <th scope="col">Name</th>
           <th scope="col">Email</th>
           <th scope="col">Phone Number</th>
           <th scope="col">Location</th>
         </tr>
       </thead>
       <tbody>
+        {employeeInfo.map((employee, index) => {
+          return (
+            <tr>
+              <td scope="column">{index + 1}</td>
+              <td className="img-container">
+                <img
+                  alt={
+                    employee?.name?.first +
+                    "," +
+                    employee?.name?.last
+                  }
+                  src={employee?.picture?.medium}
+                />
+              </td>
+              <td>{employee?.name?.first} {employee?.name?.last}</td>
+          <td>{employee?.email}</td>
+          <td>{employee?.phone}</td>
+          <td>
+            {employee?.location?.city},{" "}
+            {employee?.location?.state}
+          </td>
+            </tr>
+          );
+        })}
         <tr>
-          <th scope="row">1</th>
-
           <td className="img-container">
             {/* <img
               alt={
@@ -58,7 +80,8 @@ function EmployeeCard(employeeInfo) {
           <td>{employeeInfo[0]?.email}</td>
           <td>{employeeInfo[0]?.phone}</td>
           <td>
-            {employeeInfo[0]?.location?.city}{","}
+            {employeeInfo[0]?.location?.city}
+            {","}
             {employeeInfo[0]?.location?.state}
           </td>
         </tr>
