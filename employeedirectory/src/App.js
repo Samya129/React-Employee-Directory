44664db1d7e32f09b38a2b55ea827b5d4b6bafae
Samya Fiki <crystalgrey@Samyas-MBP.fios-router.home>
2021-03-31 22:33:11 -0400

    Modified application with cleaner code.

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,28 +1,14 @@
-import React, { useState } from "react";
-// import Header from "./components/Header";
-import Wrapper from "./components/Wrapper";
-import Api from "./utils/Api"
-import SearchForm from "./components/SearchForm";
+import React from "react";
+import Header from "./components/Header/Header.js";
+import Api from "../src/utils/API"
 import Title from "./components/Title";
 
 function App() {
-  //this is called from index.js
-  const [input, setInput] = useState("");
-
-  const handleInputChange = event => {
-    setInput(event.target.value)
-  
-  };
   return (
     <div>
-      <header className="header">
-      {/* add the className from the css style sheet */}
-      <Title>Employee Directory</Title> 
-      <SearchForm  handleInputChange={handleInputChange}/>
-    </header>
-      <Wrapper>
-      <Api employeeInput={input}/>
-    </Wrapper>
+    <Header/>
+    <Title/>
+      <Api/>
     </div>
   );
 }
